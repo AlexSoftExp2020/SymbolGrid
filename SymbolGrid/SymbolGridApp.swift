acee7abcce6287f2d6b286ef2e26b9bd25d819e2
Alex <oleksii.pidhornyi@gmail.com>
2024-12-20 14:32:41 +0100

    Initial Commit

---
+++ SymbolGrid/SymbolGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGridApp.swift
+//  SymbolGrid
+//
+//  Created by Oleksii on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
