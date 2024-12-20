3e9f6647385a26987f6dbb5d4ecc679268d656fc
Alex <oleksii.pidhornyi@gmail.com>
2024-12-20 14:37:28 +0100

    add symbol

---
+++ SymbolGrid/Symbol.swift
@@ -0,0 +1,13 @@
+//
+//  Symbol.swift
+//  SymbolGrid
+//
+//  Created by Oleksii on 20.12.2024.
+//
+
+import Foundation
+
+struct Symbol: Identifiable, Equatable {
+    var id = UUID()
+    var name: String
+}
